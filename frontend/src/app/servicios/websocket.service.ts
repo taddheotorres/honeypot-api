e3cbef5a7e99a08014412821c7fec69b63a77fe3
Taddheo Torres <taddheotorrespro@gmail.com>
2026-06-27 09:51:11 -0700

    fix: use dynamic imports for WebSocket libs

--- frontend/src/app/servicios/websocket.service.ts
+++ frontend/src/app/servicios/websocket.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Client, IMessage } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
 import { Subject } from 'rxjs';
 import { Ataque } from '../modelos/ataque.model';
 
 @Injectable({ providedIn: 'root' })
 export class WebSocketService {
-  private client!: Client;
+  private client: any;
   ataqueSubject = new Subject<Ataque>();
   estadisticasSubject = new Subject<any>();
 
-  conectar() {
+  async conectar() {
+    const [{ Client }, SockJS] = await Promise.all([
+      import('@stomp/stompjs'),
+      import('sockjs-client').then(m => m.default || m)
+    ]);
     this.client = new Client({
       webSocketFactory: () => new SockJS('/ws'),
       onConnect: () => {
-        this.client.subscribe('/tema/ataques', (msg: IMessage) => {
+        this.client.subscribe('/tema/ataques', (msg: any) => {
           this.ataqueSubject.next(JSON.parse(msg.body));
         });
-        this.client.subscribe('/tema/estadisticas', (msg: IMessage) => {
+        this.client.subscribe('/tema/estadisticas', (msg: any) => {
           this.estadisticasSubject.next(JSON.parse(msg.body));
         });
       }
